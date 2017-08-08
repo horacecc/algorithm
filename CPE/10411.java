f65d694e0faf786fcdb53d0a86e86fac68ebe4d6
Jeff Chen <jefffffff70709@gmail.com>
2017-08-08 15:23:23 +0800

    Update CPE10411

---
+++ CPE/10411.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            System.out.println(2 * v * t);
+        }
+        sc.close();
+    }
+}
