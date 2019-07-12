fb7b556937cecd3e07994fb3e64f9686fad86982
Yati <yatimisi2018@gmail.com>
2019-07-12 11:22:09 +0800

    leetcode 0021

---
+++ LeetCode/0021.go
@@ -0,0 +1,50 @@
+package solutions
+
+// ListNode Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+
+	l2.Next = mergeTwoLists(l1, l2.Next)
+
+	return l2
+}
+
+// 1->2->4
+// 1->3->4
+
+// 1->2->4
+// 1->	3->4
+
+// 1->	2->4
+// 3->4
+
+// 2->	4
+// 3->4
+
+// 4
+// 3->	4
+
+// 4
+// 4
+
+// nil
+// 4
+
+// 1->1->2->3->4->4
