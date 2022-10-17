6fc4d3e45b8bdec6f7f8e79b0b3c7a614371e8dc
Horace Chen <horacecc@horacecc.org>
2022-10-17 08:11:32 +0800

    leetcode 0605

---
+++ LeetCode/0605.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
+
+        count = 0
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                leftEmpty = (i == 0) or (flowerbed[i - 1] == 0)
+                rightEmpty = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                if leftEmpty and rightEmpty:
+                    count += 1
+                    flowerbed[i] = 1
+
+                    if count >= n:
+                        return True
+        
+        return False
+
