a9dbf9caed856e8ca468b64953d606a25b9d8c48
Horace Chen <horacecc@horacecc.org>
2022-11-02 09:27:44 +0800

    leetcode 1572

---
+++ LeetCode/1572.py
@@ -0,0 +1,10 @@
+# O(n) O(1)
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        n = len(mat)
+        res = 0
+        for i in range(n):
+            res += mat[i][i] + mat[i][n-1-i]
+        if n % 2:
+            res -= mat[n//2][n//2]
+        return res
