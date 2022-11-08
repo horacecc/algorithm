00924441a94d29ca0eae708f1ac5f2bd3c4bc00e
Horace Chen <horacecc@horacecc.org>
2022-11-08 09:12:53 +0800

    leetcode 0374

---
+++ LeetCode/0374.py
@@ -0,0 +1,29 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+# O(log n) O(1)
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, r = 1, n
+        while l <= r:
+            m = (l + r) // 2
+            g = guess(m)
+            if g == 0:
+                return m
+            elif g == 1:
+                l = m + 1
+            else:
+                r = m - 1
+        return l
+
+# # O(n) O(1)
+# class Solution:
+#     def guessNumber(self, n: int) -> int:
+#         for i in range(1, n + 1):
+#             if guess(i) == 0:
+#                 return i
+#         return n
