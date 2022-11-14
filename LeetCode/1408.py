5a0f4f1fc6f50020b3b2aaab61f0ab2bb34f96e0
Horace Chen <horacecc@horacecc.org>
2022-11-14 08:19:41 +0800

    leetcode 1408

---
+++ LeetCode/1408.py
@@ -0,0 +1,10 @@
+# O(n^2 * m) O(n)
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i != j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        return res
