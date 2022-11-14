e7c88711656813b1809a9b11ed726cc4150c2d93
Horace Chen <horacecc@horacecc.org>
2022-11-14 08:59:32 +0800

    leetcode 1486

---
+++ LeetCode/1486.py
@@ -0,0 +1,7 @@
+# O(n) O(1)
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        res = 0
+        for i in range(n):
+            res ^= start + 2 * i
+        return res
