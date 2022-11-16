650e30e4bf37fe31a604cb7333abea01ca409583
Horace Chen <horacecc@horacecc.org>
2022-11-16 08:26:16 +0800

    leetcode 0700

---
+++ LeetCode/0700.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# O(n) O(1)
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root and root.val != val:
+            root = root.left if val < root.val else root.right
+        return root
+
+# # O(n) O(n)
+# class Solution:
+#     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         if not root or root.val == val:
+#             return root
+#         return self.searchBST(root.left, val) if val < root.val else self.searchBST(root.right, val)
+
+# # O(n^2) O(n)
+# class Solution:
+#     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         if not root:
+#             return None
+#         if root.val == val:
+#             return root
+#         if val < root.val:
+#             return self.searchBST(root.left, val)
+#         return self.searchBST(root.right, val)
