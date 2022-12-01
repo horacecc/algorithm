2a3ee5e1cbeae9a96f547b9cd7c5b5c1c3c0dc04
Horace Chen <horacecc@horacecc.org>
2022-12-01 09:27:17 +0800

    leetcode 0709

--- LeetCode/0709.py
+++ LeetCode/0709.py
@@ -1,10 +1,4 @@
+# O(n) O(n)
 class Solution:
-    def toLowerCase(self, str: str) -> str:
-        ans = ""
-        for x in list(str):
-            if 97 <= ord(x) <= 122 or 65 <= ord(x) <= 90:
-                ans += chr(ord(x) % 97 % 65+97)
-            else:
-                ans += x
-        return ans
-
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
