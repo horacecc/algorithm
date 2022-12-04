f83d36e414d3122b9e13dd214834548d6f9361aa
Horace Chen <horacecc@horacecc.org>
2022-12-04 09:30:42 +0800

    leetcode 0492

---
+++ LeetCode/0492.py
@@ -0,0 +1,7 @@
+# O(sqrt(n)) O(1)
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        w = int(area**0.5)
+        while area % w:
+            w -= 1
+        return [area // w, w]
