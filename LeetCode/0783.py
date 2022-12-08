6b1598d0094f9394a2ae4b0e0045572964231a70
Horace Chen <horacecc@horacecc.org>
2022-12-08 08:12:02 +0800

    leetcode 0783

---
+++ LeetCode/0783.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# O(n) O(n)
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        vals = []
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            vals.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        res = float('inf')
+        for i in range(1, len(vals)):
+            res = min(res, vals[i] - vals[i-1])
+        return res
