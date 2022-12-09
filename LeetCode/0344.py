ca1146cadb8fe68c6ec10be3059ef45dfd3f9247
Horace Chen <horacecc@horacecc.org>
2022-12-09 08:38:11 +0800

    leetcode 0344

---
+++ LeetCode/0344.py
@@ -0,0 +1,17 @@
+# O(n) O(1)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        for i in range(n // 2):
+            s[i], s[n - 1 - i] = s[n - 1 - i], s[i]
+
+# # O(n) O(n)
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         s[:] = s[::-1]
