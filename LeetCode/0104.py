44d013df31a3e443f352b2f56be22c5a6ccc26f2
Horace Chen <horacecc@horacecc.org>
2022-12-11 09:25:59 +0800

    leetcode 0104

--- LeetCode/0104.py
+++ LeetCode/0104.py
@@ -1,11 +1,13 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
+# O(n) O(n)
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        return 1+max(self.maxDepth(root.left), self.maxDepth(root.right)) if root else 0
-
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
