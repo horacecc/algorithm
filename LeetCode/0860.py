463391778d7eb9bc1d30d88cab2827b9de225ccb
Horace Chen <horacecc@horacecc.org>
2022-12-13 09:06:07 +0800

    leetcode 0860

---
+++ LeetCode/0860.py
@@ -0,0 +1,46 @@
+# O(n) O(1)
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = ten = 0
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            else:
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
+# # O(n) O(n)
+# class Solution:
+#     def lemonadeChange(self, bills: List[int]) -> bool:
+#         five = 0
+#         ten = 0
+        
+#         for bill in bills:
+#             if bill == 5:
+#                 five += 1
+#             elif bill == 10:
+#                 if five == 0:
+#                     return False
+#                 five -= 1
+#                 ten += 1
+#             else:
+#                 if ten > 0 and five > 0:
+#                     ten -= 1
+#                     five -= 1
+#                 elif five >= 3:
+#                     five -= 3
+#                 else:
+#                     return False
+        
+#         return True
