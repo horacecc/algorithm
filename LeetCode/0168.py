53c7246034daf91823592acafcb983a23e7c4693
Horace Chen <horacecc@horacecc.org>
2022-12-20 08:42:17 +0800

    leetcode 0168

---
+++ LeetCode/0168.py
@@ -0,0 +1,9 @@
+# O(log n) O(log n)
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = ""
+        while columnNumber:
+            columnNumber -= 1
+            res = chr(columnNumber % 26 + ord('A')) + res
+            columnNumber //= 26
+        return res
