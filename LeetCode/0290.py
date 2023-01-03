3130765ed8fcf35048065dd8245b3ac833d3d5da
Horace Chen <horacecc@horacecc.org>
2023-01-03 08:49:29 +0800

    leetcode 0290

---
+++ LeetCode/0290.py
@@ -0,0 +1,7 @@
+# O(n) O(n)
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        return len(set(pattern)) == len(set(words)) == len(set(zip(pattern, words)))
