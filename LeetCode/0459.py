addb1fe63e39b179041089f6620e27ff92fec8d6
Horace Chen <horacecc@horacecc.org>
2023-01-09 08:51:15 +0800

    leetcode 0459

---
+++ LeetCode/0459.py
@@ -0,0 +1,14 @@
+# O(n) O(n)
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in (s + s)[1:-1]
+
+# # O(n^2) O(n)
+# class Solution:
+#     def repeatedSubstringPattern(self, s: str) -> bool:
+#         n = len(s)
+#         for i in range(1, n // 2 + 1):
+#             if n % i == 0:
+#                 if s[:i] * (n // i) == s:
+#                     return True
+#         return False
