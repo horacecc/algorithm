b843f73761aba2fa37de3333d4e82a9258d50b87
Horace Chen <horacecc@horacecc.org>
2023-01-16 08:08:52 +0800

    leetcode 0383

---
+++ LeetCode/0383.py
@@ -0,0 +1,9 @@
+# O(n) O(26)
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cnt = Counter(magazine)
+        for c in ransomNote:
+            if cnt[c] <= 0:
+                return False
+            cnt[c] -= 1
+        return True
