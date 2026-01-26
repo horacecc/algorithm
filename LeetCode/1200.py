ef9d6391e8d834de2c64e3cb1e480c3f1536a940
Horace Chen <h@horacecc.org>
2026-01-26 08:31:37 +0800

    leetcode 1200

--- LeetCode/1200.py
+++ LeetCode/1200.py
@@ -1,29 +1,17 @@
-# O(n log n) O(1)
+# O(n log n) O(n)
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        arr.sort()
         res = []
-        min_diff = float('inf')
-        for i in range(1, len(arr)):
-            d = arr[i] - arr[i-1]
-            if d < min_diff:
-                min_diff = d
-                res = [[arr[i-1], arr[i]]]
-            elif d == min_diff:
-                res.append([arr[i-1], arr[i]])
+        mini = float('inf')
+        arr.sort() # O(n log n)
+
+        for i in range(len(arr) - 1): # O(n)
+            mini_now = arr[i + 1] - arr[i]
+            if mini_now < mini:
+                mini = mini_now
+                res = [[arr[i], arr[i + 1]]]
+            elif mini_now == mini:
+                res.append([arr[i], arr[i + 1]])
+
         return res
 
-# # O(n log n) O(n)
-# class Solution:
-#     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-#         arr.sort()
-#         diffs = []
-#         for i in range(1, len(arr)):
-#             diffs.append((arr[i] - arr[i-1], arr[i-1], arr[i]))
-#         diffs.sort()
-#         min_diff = diffs[0][0]
-#         res = []
-#         for d, a, b in diffs:
-#             if d == min_diff:
-#                 res.append([a, b])
-#         return res
