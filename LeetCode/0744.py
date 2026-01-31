7e36403029e386802aecba8ac4d9a4223a774c8f
Horace Chen <h@horacecc.org>
2026-01-31 09:03:26 +0800

    leetcode 0744

--- LeetCode/0744.py
+++ LeetCode/0744.py
@@ -1,19 +1,33 @@
 # O(log n) O(1)
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        l, r = 0, len(letters)
-        while l < r:
-            m = (l + r) // 2
-            if letters[m] <= target:
-                l = m + 1
+        left, right = 0, len(letters) - 1
+        
+        if target >= letters[-1]:
+            return letters[0]
+        
+        while left < right:
+            mid = (left + right) // 2
+            if letters[mid] <= target:
+                left = mid + 1
             else:
-                r = m
-        return letters[l % len(letters)]
+                right = mid
+        
+        return letters[left]
 
 # # O(n) O(1)
 # class Solution:
 #     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-#         for c in letters:
-#             if c > target:
-#                 return c
-#         return letters[0]
+#         target_ascii = ord(target)
+#         res = float('inf')
+        
+#         for letter in letters:
+#             letter_ascii = ord(letter)
+#             if letter_ascii > target_ascii:
+#                 res = min(letter_ascii, res)
+        
+#         if res == float('inf'):
+#             return letters[0]
+        
+#         return chr(res)
+
