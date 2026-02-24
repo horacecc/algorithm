7fd64f3ab0a48fc8388cfa776fdfd784ae6f2c07
Horace Chen <h@horacecc.org>
2026-02-24 14:05:16 +0800

    leetcode 1022

--- LeetCode/1022.py
+++ LeetCode/1022.py
@@ -9,15 +9,31 @@
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
         res = 0
-        def dfs(node, val):
-            nonlocal res
-            if not node:
-                return
-            val = (val << 1) | node.val
-            if not node.left and not node.right:
-                res += val
-                return
-            dfs(node.left, val)
-            dfs(node.right, val)
-        dfs(root, 0)
+        stk = [(root, 0)]
+        while stk:
+            nd, cur = stk.pop()
+            cur = (cur << 1) | nd.val
+            if not nd.left and not nd.right:
+                res += cur
+            if nd.right:
+                stk.append((nd.right, cur))
+            if nd.left:
+                stk.append((nd.left, cur))
         return res
+
+# # O(n) O(n)
+# class Solution:
+#     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+#         res = 0
+#         def dfs(node, val):
+#             nonlocal res
+#             if not node:
+#                 return
+#             val = (val << 1) | node.val
+#             if not node.left and not node.right:
+#                 res += val
+#                 return
+#             dfs(node.left, val)
+#             dfs(node.right, val)
+#         dfs(root, 0)
+#         return res
