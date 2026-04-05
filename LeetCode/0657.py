f58b0d9536b530d2dea48a7806c0ca0b65958b06
Horace Chen <h@horacecc.org>
2026-04-06 04:12:06 +0800

    leetcode 0657

--- LeetCode/0657.py
+++ LeetCode/0657.py
@@ -1,8 +1,24 @@
 # O(n) O(1)
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        d = {'U': 1j, 'D': -1j, 'L': -1, 'R': 1}
-        return sum(d[m] for m in moves) == 0
+        x = y = 0
+        for move in moves:
+            match move:
+                case "R":
+                    x += 1
+                case "L":
+                    x -= 1
+                case "U":
+                    y += 1
+                case "D":
+                    y -= 1
+        return x == 0 and y == 0
+
+# # O(n) O(1)
+# class Solution:
+#     def judgeCircle(self, moves: str) -> bool:
+#         d = {'U': 1j, 'D': -1j, 'L': -1, 'R': 1}
+#         return sum(d[m] for m in moves) == 0
 
 # # O(n) O(n)
 # class Solution:
